21fff81eee8d475f03d216eae476fc58ac7a695d
Nadezhda Pak <bombme247@gmail.com>
2023-09-25 00:01:42 +0900

    feat: add shuffle and repeat functions

--- audioPlayer/js/script.js
+++ audioPlayer/js/script.js
@@ -1,38 +1,41 @@
 const background = document.querySelector("#background"),
-      trackImg = document.querySelector("#track__image"),
-      songTitle = document.querySelector("#title"),
-      artist = document.querySelector("#artist"),
-      currentAudio = document.querySelector("#current-song"),
-      playPauseBtn = document.querySelector(".play-pause"),
-      prevBtn = document.querySelector(".back"),
-      nextBtn = document.querySelector(".forward"),
-      progressLine = document.querySelector(".line");
+  trackImg = document.querySelector("#track__image"),
+  songTitle = document.querySelector("#title"),
+  artist = document.querySelector("#artist"),
+  currentAudio = document.querySelector("#current-song"),
+  playPauseBtn = document.querySelector(".play-pause"),
+  prevBtn = document.querySelector(".back"),
+  nextBtn = document.querySelector(".forward"),
+  durationLine = document.querySelector(".duration__line"),
+  progressLine = document.querySelector(".line"),
+  repeatBtn = document.querySelector(".repeat"),
+  shuffleBtn = document.querySelector(".shuffle");
 
-      let trackIndex = 1;
+let trackIndex = 1;
 
-      window.addEventListener("load", () => {
-        loadTrack(trackIndex);
-      })
+window.addEventListener("load", () => {
+  loadTrack(trackIndex);
+});
 
-// load song function  
+// load song function
 function loadTrack(index) {
   background.src = `assets/images/${allSongs[index - 1].img}.jpg`;
   trackImg.src = `assets/images/${allSongs[index - 1].img}.jpg`;
   songTitle.innerText = allSongs[index - 1].title;
-  artist.innerText = allSongs[index - 1].artist,
-  currentAudio.src = `assets/songs/${allSongs[index - 1].src}.m4a`;
-};
+  (artist.innerText = allSongs[index - 1].artist),
+    (currentAudio.src = `assets/songs/${allSongs[index - 1].src}.m4a`);
+}
 
-// change class function 
+// change class function
 function replaceClass(id, oldClass, newClass) {
   const element = document.getElementById(id);
   element.classList.remove(oldClass);
   element.classList.add(newClass);
-};
+}
 
 let isPlay = false;
 
-// play song function 
+// play song function
 function playAudio() {
   isPlay = true;
   currentAudio.play();
@@ -44,49 +47,48 @@ function pauseAudio() {
   currentAudio.pause();
 }
 
-// next audio function 
+// next audio function
 function nextAudio() {
   trackIndex++;
-  // after the last audio go back to the first one 
-  trackIndex > allSongs.length ? trackIndex = 1 : trackIndex = trackIndex;
+  // after the last audio go back to the first one
+  trackIndex > allSongs.length ? (trackIndex = 1) : (trackIndex = trackIndex);
   loadTrack(trackIndex);
   playAudio();
 }
 
-// previous audio function 
+// previous audio function
 function prevAudio() {
   trackIndex--;
   loadTrack(trackIndex);
   playAudio();
 }
 
-// ===== Event listeners =====
+// change play-pause button function
+function changePlayBtn() {
+  isPlay ? replaceClass("playPause", "fa-play", "fa-pause") : replaceClass("playPause", "fa-pause", "fa-play");
+}
 
-// if play or pause button event 
+// ============ Event listeners ============
+
+// if play or pause button event
 playPauseBtn.addEventListener("click", () => {
   isPlay ? pauseAudio() : playAudio();
-  isPlay
-    ? replaceClass("playPause", "fa-play", "fa-pause")
-    : replaceClass("playPause", "fa-pause", "fa-play");
+  changePlayBtn();
 });
 
-// next audio button event 
+// next audio button event
 nextBtn.addEventListener("click", () => {
   nextAudio();
-  isPlay
-  ? replaceClass("playPause", "fa-play", "fa-pause")
-  : replaceClass("playPause", "fa-pause", "fa-play");
+  changePlayBtn();
 });
 
-// previous audio button event 
+// previous audio button event
 prevBtn.addEventListener("click", () => {
   prevAudio();
-  isPlay
-  ? replaceClass("playPause", "fa-play", "fa-pause")
-  : replaceClass("playPause", "fa-pause", "fa-play");
+  changePlayBtn();
 });
 
-// duration line changing according to audio 
+// duration line changing according to audio
 currentAudio.addEventListener("timeupdate", (e) => {
   const currentTime = e.target.currentTime;
   const duration = e.target.duration;
@@ -96,7 +98,7 @@ currentAudio.addEventListener("timeupdate", (e) => {
   let audioCurrentTime = document.querySelector(".current");
   let audioDurationTime = document.querySelector(".duration");
 
-  // update audio total duration time
+  // update audio-total-duration time
   currentAudio.addEventListener("loadeddata", () => {
     let audioDuration = currentAudio.duration;
     let totalMin = Math.floor(audioDuration / 60);
@@ -106,7 +108,7 @@ currentAudio.addEventListener("timeupdate", (e) => {
     }
     audioDurationTime.innerText = `${totalMin}:${totalSec}`;
   });
-  // update audio progress time
+  // update audio-progress-time
   let currentMin = Math.floor(currentTime / 60);
   let currentSec = Math.floor(currentTime % 60);
   if (currentSec < 10) {
@@ -115,3 +117,94 @@ currentAudio.addEventListener("timeupdate", (e) => {
   audioCurrentTime.innerText = `${currentMin}:${currentSec}`;
 });
 
+// change progress-time by clicking on line
+durationLine.addEventListener("click", (e) => {
+  let progressLineWidth = durationLine.clientWidth; //width of duration line
+  let clickedOffSetX = e.offsetX; //offset x value
+  let trackDuration = currentAudio.duration;
+
+  currentAudio.currentTime =
+    (clickedOffSetX / progressLineWidth) * trackDuration;
+  playAudio();
+  changePlayBtn();
+});
+
+// change repeat-icon color
+repeatBtn.addEventListener("click", () => {
+  const repeatId = document.getElementById("repeat-id");
+  let repeatColor = repeatId.getAttribute("style");
+  const shuffleId = document.getElementById("shuffle-id");
+  let shuffleColor = shuffleId.getAttribute("style");
+
+  switch (repeatColor) {
+    case "color: #eeeeee;":
+      repeatId.setAttribute("style", "color: #ff4734;");
+      shuffleId.setAttribute("style", "color: #eeeeee;");
+      break;
+    case "color: #ff4734;":
+      repeatId.setAttribute("style", "color: #eeeeee;");
+      break;
+  }
+});
+
+// function repeat-audio-or-not clicking on repeat button
+function repeatOrNot() {
+  const repeatId = document.getElementById("repeat-id");
+  let repeatColor = repeatId.getAttribute("style");
+
+  switch (repeatColor) {
+    case "color: #eeeeee;":
+      nextAudio();
+      break;
+    case "color: #ff4734;":
+      currentAudio.currentTime = 0;
+      loadTrack(trackIndex);
+      playAudio();
+      break;
+  }
+}
+
+// what's happening with the song according to the icon (repeat or not)
+
+// change shuffle icon color
+shuffleBtn.addEventListener("click", () => {
+  const shuffleId = document.getElementById("shuffle-id");
+  let shuffleColor = shuffleId.getAttribute("style");
+  const repeatId = document.getElementById("repeat-id");
+  let repeatColor = repeatId.getAttribute("style");
+
+  switch (shuffleColor) {
+    case "color: #eeeeee;":
+      shuffleId.setAttribute("style", "color: #ff4734;");
+      repeatId.setAttribute("style", "color: #eeeeee;")
+      break;
+    case "color: #ff4734;":
+      shuffleId.setAttribute("style", "color: #eeeeee;");
+      break;
+  }
+});
+
+// making a shuffle-button function
+function shuffleOrNot() {
+    const shuffleId = document.getElementById("shuffle-id");
+    let shuffleColor = shuffleId.getAttribute("style");
+
+    switch (shuffleColor) {
+      case "color: #eeeeee;":
+          repeatOrNot();
+        break;
+      case "color: #ff4734;":
+        let randomIndex = Math.floor((Math.random() * allSongs.length) + 1);
+        do {
+          randomIndex = Math.floor((Math.random() * allSongs.length) + 1);
+        } while (trackIndex == randomIndex);
+        trackIndex = randomIndex;
+        loadTrack(trackIndex);
+        playAudio();
+        break;
+    }
+}
+
+currentAudio.addEventListener("ended", () => {
+  shuffleOrNot();
+});
